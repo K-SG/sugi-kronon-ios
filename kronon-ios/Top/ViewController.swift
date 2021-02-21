0da70d9056f847b1f490cef0f43a17c03ca4d12d
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2021-02-21 15:17:38 +0900

    TOP画面からログイン画面を表示できるように。スタート画面を横でも表示できるように。

--- kronon-ios/Top/ViewController.swift
+++ kronon-ios/Top/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func start_button(_ sender: UIButton) {
+//        let viewController = LoginViewController()
+//        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
 }
 
