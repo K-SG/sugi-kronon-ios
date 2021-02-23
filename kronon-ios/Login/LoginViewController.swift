1f873df326a216122488b4b5369b12503a494ca0
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2021-02-23 15:07:53 +0900

    commit戻し

--- kronon-ios/Login/LoginViewController.swift
+++ kronon-ios/Login/LoginViewController.swift
@@ -9,23 +9,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-//    @IBOutlet weak var textField:UITextField!
-    
-    @IBOutlet weak var mail_text: UITextField!
-    @IBOutlet weak var password_text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let mailLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
-        let passwordLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
-//        mailLeftPadding.backgroundColor = UIColor.clear
-        mail_text.leftView = mailLeftPadding
-        mail_text.leftViewMode = .always
-        password_text.leftView = passwordLeftPadding
-        password_text.leftViewMode = .always
     }
-    
 
 
     /*
