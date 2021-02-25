53f7ceaae7c445950aa5b018f58f256a55d8ba03
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2021-02-25 23:14:56 +0900

    アカウント作成画面作成。警告出ます。

--- kronon-ios/NewAccount/NewAccountViewController.swift
+++ kronon-ios/NewAccount/NewAccountViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class NewAccountViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var nameEditText: UITextField!
+    @IBOutlet weak var emailEditText: UITextField!
+    @IBOutlet weak var passwordEditText: UITextField!
+    @IBOutlet weak var passwordConfirmEditText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let nameLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        let emailLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        let passwordLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        let passwordConfirmLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+//
+        nameEditText.leftView = nameLeftPadding
+        nameEditText.leftViewMode = .always
+        emailEditText.leftView = emailLeftPadding
+        emailEditText.leftViewMode = .always
+        passwordEditText.leftView = passwordLeftPadding
+        passwordEditText.leftViewMode = .always
+        passwordConfirmEditText.leftView = passwordConfirmLeftPadding
+        passwordConfirmEditText.leftViewMode = .always
         // Do any additional setup after loading the view.
+        
     }
     
 
