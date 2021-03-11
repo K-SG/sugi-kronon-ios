7d723337cffb3bbb1e26d471b3d2d5b294a8e94c
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2021-03-11 16:45:48 +0900

    予定登録の画面遷移作成

--- kronon-ios/addScehdule/addScheduleViewController.swift
+++ kronon-ios/addScehdule/addScheduleViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
-class addScheduleViewController: UIViewController {
+class addScheduleViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var okButton: UIButton!
+    let dataList = [
+        "オフィス","外出","在宅"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Delegate設定
+        pickerView.delegate = self
+        pickerView.dataSource = self
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -19,6 +27,29 @@ class addScheduleViewController: UIViewController {
         self.parent?.navigationItem.title = "予定登録"
         super.viewWillAppear(animated)
     }
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    // UIPickerViewの行数、リストの数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
+    }
+    // UIPickerViewの最初の表示
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        return dataList[row]
+    }
+    @IBAction func okButton(_ sender: Any) {
+        print("OKbuttons")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
     
 
     /*
