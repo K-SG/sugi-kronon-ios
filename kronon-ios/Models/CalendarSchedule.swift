bd426dad0f23afd61582bd4f1c7a5ee32fe5e9f7
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2021-03-13 17:56:25 +0900

    カレンダー作成

---
+++ kronon-ios/Models/CalendarSchedule.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarSchedule.swift
+//  kronon-ios
+//
+//  Created by 杉浩輝 on 2021/03/13.
+//
+
+import Foundation
+
+class CalendarSchedule {
+    var date:String?
+    var startTime:String?
+    var title:String?
+}
---
+++ kronon-ios/Models/Schedule.swift
@@ -0,0 +1,31 @@
+//
+//  Schedule.swift
+//  kronon-ios
+//
+//  Created by 杉浩輝 on 2021/03/12.
+//
+
+import Foundation
+
+class Schedule{
+    var userId:Int?
+    var scheduleId:Int?
+    var scheduleDate:String?
+    var title:String?
+    var startTime:Int?
+    var endTime:Int?
+    var place:Int?
+    
+    
+//    init(userid: Int, scheduleid: Int,scheduldate:String,scheduletitle:String,starttime:Int,endtime:Int,scheduleplace:Int) {
+//        self.userId = userid
+//        self.scheduleId = scheduleid
+//        self.scheduleDate = scheduldate
+//        self.title = scheduletitle
+//        self.startTime = starttime
+//        self.endTime = endtime
+//        self.place = scheduleplace
+//    }
+//
+    
+}
